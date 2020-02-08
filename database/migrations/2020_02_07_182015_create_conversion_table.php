f93bfef28b0967c7931cd03d2c4640d3a0d2a791
ahmedswa68 <ahmedswar68@gmail.com>
2020-02-08 14:31:05 +0200

    most_attracted_platform unit test

--- database/migrations/2020_02_07_182015_create_conversion_table.php
+++ database/migrations/2020_02_07_182015_create_conversion_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateConversionsTable extends Migration
+class CreateConversionTable extends Migration
 {
   /**
    * Run the migrations.
@@ -15,12 +15,13 @@ class CreateConversionsTable extends Migration
   {
     Schema::create('conversions', function (Blueprint $table) {
       $table->increments('id');
-      $table->unsignedInteger('customer_id');
-      $table->unsignedInteger('revenue');
-      $table->string('booking_reference');
-      $table->date('date_of_contact');
+      $table->unsignedBigInteger('customer_id');
       $table->string('platform');
+      $table->unsignedInteger('revenue');
+      $table->string('booking_number',50);
+      $table->date('date_of_contact');
       $table->timestamps();
+      $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
     });
   }
 
